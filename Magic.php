352a9d39be33e626356d6ce821e6f059c8f90f17
Brock Riemenschneider <brock@mvpdesign.com>
2014-11-14 14:37:47 -0600

    Stubbed out Magic class

---
+++ Magic.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MVPDesign\WordpressInstaller;
+
+use Composer\Script\Event;
+
+class Magic
+{
+    public static function happens(Event $event)
+    {
+
+    }
+
+    public static function askQuestions()
+    {
+
+    }
+
+    public static function createEnvironment()
+    {
+
+    }
+
+    public static function generateSalts()
+    {
+
+    }
+}
